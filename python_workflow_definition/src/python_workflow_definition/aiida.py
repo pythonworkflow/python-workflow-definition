6f0dec152aa242a00c626f2a3b1b4153d8bba967
Xing Wang <xingwang1991@gmail.com>
2025-04-02 14:34:57 +0200

    Replace pythonjob task by normal task, drop usage of `pickle_node` (#35)
    * Use pyfunction
    
    * use int for node name in the edges
    
    * Run the relevant notebooks
    
    * Bump aiida-workgraph to 0.5.1
    
    * Install AiiDA WG from pip in environment.yml
    
    * fix environment.yml
    
    * Also change aiida_simple workflow to use new setup.
    
    ---------
    
    Co-authored-by: Julian Geiger <julian.geiger@psi.ch>

--- python_workflow_definition/src/python_workflow_definition/aiida.py
+++ python_workflow_definition/src/python_workflow_definition/aiida.py
@@ -1,13 +1,10 @@
 from importlib import import_module
 import traceback
 from aiida_workgraph import WorkGraph, task
+from aiida_pythonjob.data.serializer import general_serializer
 import json
 from aiida import orm
 
-@task.pythonjob()
-def pickle_node(value):
-    """Handle data nodes"""
-    return value
 
 
 def load_workflow_json(file_name):
@@ -21,90 +18,65 @@ def load_workflow_json(file_name):
         if isinstance(identifier, str) and "." in identifier:
             p, m = identifier.rsplit(".", 1)
             mod = import_module(p)
-            _func = getattr(mod, m)
-            func = task.pythonjob()(_func)
-            # I use the register_pickle_by_value, because the function is defined in a local file
-            wg.add_task(func, register_pickle_by_value=True)
-
+            func = getattr(mod, m)
+            wg.add_task(func)
             # Remove the default result output, because we will add the outputs later from the data in the link
             del wg.tasks[-1].outputs["result"]
+            task_name_mapping[id] = wg.tasks[-1]
         else:
             # data task
-            wg.add_task(pickle_node, value=identifier)
+            data_node = general_serializer(identifier)
+            task_name_mapping[id] = data_node
 
-        task_name_mapping[id] = wg.tasks[-1].name
     # add links
     for link in data["edges"]:
-        if link["sh"] is None:
-            link["sh"] = "result"
-        try:
-            from_task = wg.tasks[task_name_mapping[str(link["sn"])]]
-            # because we are not define the outputs explicitly during the pythonjob creation
-            # we add it here, and assume the output exit
-            if link["sh"] not in from_task.outputs:
-            # if str(link["sh"]) not in from_task.outputs:
-                from_socket = from_task.add_output(
-                    "workgraph.any",
-                    name=link["sh"],
-                    # name=str(link["sh"]),
-                    metadata={"is_function_output": True},
-                )
-            else:
-                from_socket = from_task.outputs[link["sh"]]
-            to_task = wg.tasks[task_name_mapping[str(link["tn"])]]
-            # if the input is not exit, it means we pass the data into to the kwargs
-            # in this case, we add the input socket
-            if link["th"] not in to_task.inputs:
-                #
-                to_socket = to_task.add_input(
-                    "workgraph.any",
-                    name=link["th"],
-                    metadata={"is_function_input": True},
-                )
-            else:
-                to_socket = to_task.inputs[link["th"]]
-            wg.add_link(from_socket, to_socket)
-        except Exception as e:
-            traceback.print_exc()
-            print("Failed to link", link, "with error:", e)
+        to_task = task_name_mapping[str(link["tn"])]
+        # if the input is not exit, it means we pass the data into to the kwargs
+        # in this case, we add the input socket
+        if link["th"] not in to_task.inputs:
+            to_socket = to_task.add_input( "workgraph.any", name=link["th"])
+        else:
+            to_socket = to_task.inputs[link["th"]]            
+        from_task = task_name_mapping[str(link["sn"])]
+        if isinstance(from_task, orm.Data):
+            to_socket.value = from_task
+        else:
+            try:
+                if link["sh"] is None:
+                    link["sh"] = "result"
+                # because we are not define the outputs explicitly during the pythonjob creation
+                # we add it here, and assume the output exit
+                if link["sh"] not in from_task.outputs:
+                # if str(link["sh"]) not in from_task.outputs:
+                    from_socket = from_task.add_output(
+                        "workgraph.any",
+                        name=link["sh"],
+                        # name=str(link["sh"]),
+                        metadata={"is_function_output": True},
+                    )
+                else:
+                    from_socket = from_task.outputs[link["sh"]]
+
+                wg.add_link(from_socket, to_socket)
+            except Exception as e:
+                traceback.print_exc()
+                print("Failed to link", link, "with error:", e)
     return wg
 
 
 def write_workflow_json(wg, file_name):
+    from aiida_workgraph.socket import TaskSocketNamespace
     data = {"nodes": {}, "edges": []}
     node_name_mapping = {}
+    data_node_name_mapping = {}
     i = 0
     for node in wg.tasks:
         executor = node.get_executor()
         node_name_mapping[node.name] = i
 
         callable_name = executor["callable_name"]
-
-        if callable_name == "pickle_node":
-            input_value = data["nodes"][str(i)] = node.inputs.value.value
-            try:
-                if isinstance(input_value, orm.Data):
-                    if isinstance(input_value, orm.List):
-                        data["nodes"][str(i)] = input_value.get_list()
-                    elif isinstance(input_value, orm.Dict):
-                        data["nodes"][str(i)] = input_value.get_dict()
-                    else:
-                        data["nodes"][str(i)] = input_value.value
-                else:
-                    data["nodes"][str(i)] = input_value
-            except:
-                import traceback
-
-                traceback.print_stack()
-                raise
-                # raise
-                # import ipdb; ipdb.set_trace()
-
-        else:
-            callable_name = f"{executor['module_path']}.{callable_name}"
-
-            data["nodes"][str(i)] = callable_name
-
+        callable_name = f"{executor['module_path']}.{callable_name}"
+        data["nodes"][str(i)] = callable_name
         i += 1
 
     for link in wg.links:
@@ -117,7 +89,34 @@ def write_workflow_json(wg, file_name):
         link_data["sn"] = node_name_mapping[link_data.pop("from_node")]
         link_data["sh"] = link_data.pop("from_socket")
         data["edges"].append(link_data)
-
+    
+    for node in wg.tasks:
+        for input in node.inputs:
+            # assume namespace is not used as input
+            if isinstance(input, TaskSocketNamespace):
+                continue
+            if isinstance(input.value, orm.Data):
+                if input.value.uuid not in data_node_name_mapping:
+                    if isinstance(input.value, orm.List):
+                        raw_value = input.value.get_list()
+                    elif isinstance(input.value, orm.Dict):
+                        raw_value = input.value.get_dict()
+                        # unknow reason, there is a key "node_type" in the dict
+                        raw_value.pop("node_type", None)
+                    else:
+                        raw_value = input.value.value
+                    data["nodes"][str(i)] = raw_value
+                    input_node_name = i
+                    data_node_name_mapping[input.value.uuid] = input_node_name
+                    i += 1
+                else:
+                    input_node_name = data_node_name_mapping[input.value.uuid]
+                data["edges"].append({
+                    "tn": node_name_mapping[node.name],
+                    "th": input._name,
+                    "sn": input_node_name,
+                    "sh": None
+                })
     with open(file_name, "w") as f:
         # json.dump({"nodes": data[], "edges": edges_new_lst}, f)
         json.dump(data, f, indent=2)
