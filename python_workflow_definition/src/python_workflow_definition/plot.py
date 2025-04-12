2ef0f010240e3956bc4cecfe1763cd021cfadf90
Jan Janßen <janssen@mpie.de>
2025-04-12 12:40:43 +0200

    Plot notebooks

--- python_workflow_definition/src/python_workflow_definition/plot.py
+++ python_workflow_definition/src/python_workflow_definition/plot.py
@@ -4,17 +4,24 @@ from IPython.display import SVG, display
 import networkx as nx
 
 
-from python_workflow_definition.shared import get_kwargs, convert_nodes_list_to_dict
 from python_workflow_definition.purepython import group_edges
+from python_workflow_definition.shared import (
+    get_kwargs,
+    convert_nodes_list_to_dict,
+    NODES_LABEL,
+    EDGES_LABEL,
+    SOURCE_LABEL,
+    SOURCE_PORT_LABEL,
+)
 
 
-def plot(json_file_name):
-    with open(json_file_name, "r") as f:
+def plot(file_name):
+    with open(file_name, "r") as f:
         content = json.load(f)
 
     graph = nx.DiGraph()
-    node_dict = convert_nodes_list_to_dict(nodes_list=content["nodes"])
-    total_lst = group_edges(edges_lst=content["edges"])
+    node_dict = convert_nodes_list_to_dict(nodes_list=content[NODES_LABEL])
+    total_lst = group_edges(edges_lst=content[EDGES_LABEL])
 
     for node_id, node_name in node_dict.items():
         graph.add_node(node_id, name=str(node_name), label=str(node_name))
@@ -23,12 +30,12 @@ def plot(json_file_name):
         target_node, edge_dict = edge_tuple
         edge_label_dict = {}
         for k, v in edge_dict.items():
-            if v["source"] not in edge_label_dict:
-                edge_label_dict[v["source"]] = []
-            if v["sourcePort"] is None:
-                edge_label_dict[v["source"]].append(k)
+            if v[SOURCE_LABEL] not in edge_label_dict:
+                edge_label_dict[v[SOURCE_LABEL]] = []
+            if v[SOURCE_PORT_LABEL] is None:
+                edge_label_dict[v[SOURCE_LABEL]].append(k)
             else:
-                edge_label_dict[v["source"]].append(v["sourcePort"] + "=" + k)
+                edge_label_dict[v[SOURCE_LABEL]].append(v[SOURCE_PORT_LABEL] + "=" + k)
         for k, v in edge_label_dict.items():
             graph.add_edge(str(k), str(target_node), label=", ".join(v))
 
