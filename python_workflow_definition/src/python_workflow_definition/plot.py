e13b66baeef569b4e80cd6b61bb5b875e14d3dc6
Jan Janßen <janssen@mpie.de>
2025-04-12 11:07:55 +0200

    Add plotting function

---
+++ python_workflow_definition/src/python_workflow_definition/plot.py
@@ -0,0 +1,36 @@
+import json
+
+from IPython.display import SVG, display
+import networkx as nx
+
+
+from python_workflow_definition.shared import get_kwargs, convert_nodes_list_to_dict
+from python_workflow_definition.purepython import group_edges
+
+
+def plot(json_file_name):
+    with open(json_file_name, "r") as f:
+        content = json.load(f)
+
+    graph = nx.DiGraph()
+    node_dict = convert_nodes_list_to_dict(nodes_list=content["nodes"])
+    total_lst = group_edges(edges_lst=content["edges"])
+
+    for node_id, node_name in node_dict.items():
+        graph.add_node(node_id, name=str(node_name), label=str(node_name))
+
+    for edge_tuple in total_lst:
+        target_node, edge_dict = edge_tuple
+        edge_label_dict = {}
+        for k, v in edge_dict.items():
+            if v["source"] not in edge_label_dict:
+                edge_label_dict[v["source"]] = []
+            if v["sourcePort"] is None:
+                edge_label_dict[v["source"]].append(k)
+            else:
+                edge_label_dict[v["source"]].append(v["sourcePort"] + "=" + k)
+        for k, v in edge_label_dict.items():
+            graph.add_edge(str(k), str(target_node), label=", ".join(v))
+
+    svg = nx.nx_agraph.to_agraph(graph).draw(prog="dot", format="svg")
+    display(SVG(svg))
