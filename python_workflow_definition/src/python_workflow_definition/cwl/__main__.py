50a72c98111f940f0e03c26a9b6478a0e00b809b
Jan Janßen <janssen@mpie.de>
2025-05-13 21:36:56 +0200

    Implement export to cwl

---
+++ python_workflow_definition/src/python_workflow_definition/cwl/__main__.py
@@ -0,0 +1,42 @@
+import sys
+import pickle
+from ast import literal_eval
+from importlib import import_module
+
+
+def load_function(funct):
+    p, m = funct.rsplit('.', 1)
+    return getattr(import_module(p), m)
+
+
+def convert_argument(arg):
+    if ".pickle" in arg:
+        with open(arg, 'rb') as f:
+            return pickle.load(f)
+    else:
+        return literal_eval(arg)
+
+
+if __name__ == "__main__":
+    # load input
+    argument_lst = sys.argv[1:]
+    funct = [
+        load_function(funct=arg.split("=")[-1])
+        for arg in argument_lst if "--function=" in arg
+    ][0]
+    kwargs = {
+        arg.split("=")[0][6:]: convert_argument(arg=arg.split("=")[-1])
+        for arg in argument_lst if "--arg_" in arg
+    }
+
+    # evaluate function
+    result = funct(**kwargs)
+
+    # store output
+    if isinstance(result, dict):
+        for k, v in result.items():
+            with open(k + ".pickle", 'wb') as f:
+                pickle.dump(v, f)
+    else:
+        with open("result.pickle", 'wb') as f:
+            pickle.dump(result, f)
